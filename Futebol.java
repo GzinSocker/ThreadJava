b680e046ed5358c72f25072d6f25892313248d30
GzinSocker <gabriel.cama@outlook.com>
2019-06-02 17:04:34 -0300

    Added code

---
+++ Futebol.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.Date;
+import java.util.Random;
+
+/**
+ *
+ * @author _CASTILIN_
+ */
+public class Futebol extends Time{
+    private String escudo;
+        Random r  = new Random();
+
+    public Futebol(){
+        dataCriacao = new Date();
+    }
+    public Futebol(String escudo, int numeroJogadores, String sede, Date dataCriacao){
+        this.escudo = escudo;
+        this.numeroJogadores = numeroJogadores;
+        this.sede = sede;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getEscudo() {
+        return escudo;
+    }
+
+    public void setEscudo(String escudo) {
+        this.escudo = escudo;
+    }
+    
+    public String joga(){
+        dataCriacao.setMonth(r.nextInt(12)+1);
+        dataCriacao.setDate(r.nextInt(30)+1);
+        return dataCriacao.toString();
+    }
+    public String imprime(){
+        return "vc chamou a funcao imprime";
+    }
+}
---
+++ Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.Random;
+
+/**
+ *
+ * @author _CASTILIN_
+ */
+
+public class Main {
+    public static void main(String [] args){
+        Time[] t = new Time[5];
+        Random r = new Random();
+        for(int i=0;i<5;i++){
+            if(r.nextInt(2)==0){
+                t[i] = new Futebol();
+                System.out.println((i+1)+"º time. Proximo jogo: "+t[i].joga());
+            }
+            else t[i]=new Basquete();
+        }
+        
+    }
+}
